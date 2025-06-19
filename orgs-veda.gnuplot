f32664c4ffcf109c995a62047dd648b5a4c4c0f1
Jason Maassen <jmaassen73@gmail.com>
2025-06-19 13:59:44 +0200

    Added additional RSD instances

---
+++ orgs-veda.gnuplot
@@ -0,0 +1,12 @@
+set terminal png size 1200,600 enhanced font "Helvetica,12"
+set output 'orgs-veda.png'
+set title "RSD Users per organization (VEDA)"
+set boxwidth 0.5
+set style fill solid
+set offsets 0, 0, 10, 0
+set yrange [0:]
+set grid ytics
+set xtics out 
+set xtics rotate by -45
+plot "org-veda.data" using 1:3:xtic(2) with boxes lc "web-blue" notitle, \
+'' u 0:3:3 with labels font "Helvetica,14" offset 0.1,0.5 notitle
--- plot-stats.sh
+++ plot-stats.sh
@@ -10,3 +10,19 @@ java -cp ./target/rsd-0.1-jar-with-dependencies.jar nl.esciencecenter.rsd.Main h
 echo "Plotting helmholtz graphs"
 gnuplot < users-helmholtz.gnuplot
 gnuplot < orgs-helmholtz.gnuplot
+
+java -cp ./target/rsd-0.1-jar-with-dependencies.jar nl.esciencecenter.rsd.Main nfdi https://nfdi.software
+echo "Plotting nfdi graphs"
+gnuplot < users-nfdi.gnuplot
+gnuplot < orgs-nfdi.gnuplot
+
+java -cp ./target/rsd-0.1-jar-with-dependencies.jar nl.esciencecenter.rsd.Main veda https://vedaresearch.nl
+echo "Plotting veda graphs"
+gnuplot < users-veda.gnuplot
+gnuplot < orgs-veda.gnuplot
+
+java -cp ./target/rsd-0.1-jar-with-dependencies.jar nl.esciencecenter.rsd.Main koeln https://research-software.cceh.uni-koeln.de/
+echo "Plotting koeln graphs"
+gnuplot < users-koeln.gnuplot
+gnuplot < orgs-koeln.gnuplot
+
